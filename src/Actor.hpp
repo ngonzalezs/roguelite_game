60fe189edab6b8339e4ebaaeb72b29cd8ecc0376
ngonzalezs <ngonzalezs01@informatica.iesvalledeljerteplasencia.es>
2021-12-02 19:27:03 +0100

    Creacion de la clase Actor

---
+++ src/Actor.hpp
@@ -0,0 +1,14 @@
+#include "libtcod.hpp"
+
+
+class Actor {
+public :
+   int x,y; // position on map
+   int ch; // ascii code
+   TCODColor col; // color
+
+   Actor(int x, int y, int ch, const TCODColor &col);
+   void render() const;
+};
+
+
